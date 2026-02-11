6d1dfec530d13d6ec063c5f80d472b59a418aec4
Fábio Garcia <fabio@unipar.br>
2026-02-11 16:45:34 -0300

    Versão 1.0.0.2

--- chef-ia/App.js
+++ chef-ia/App.js
@@ -1,20 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { TextInput } from 'react-native/types_generated/index';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View>
       <StatusBar style="auto" />
+      {/*Header*/}
+      <View>
+        <Text>🧑‍🍳</Text>
+        <Text>Chef IA Garcia</Text>
+        <Text>Digite os ingredientes que você tem</Text>
+      </View>
+    {/*Input*/}
+      <TextInput>
+        
+      </TextInput>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
