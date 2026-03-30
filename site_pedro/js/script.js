c83d4208aa3dcd7086357b379b49de4c58b00df7
Fábio Garcia <garciafabioha@gmail.com>
2026-03-30 11:55:04 -0300

    Versão 1.0.0.9

--- site_pedro/js/script.js
+++ site_pedro/js/script.js
@@ -13,3 +13,20 @@ function abrirMenu() {
         botao.innerHTML = "✖";
     }
 }
+
+let index = 0;
+const imagens = document.querySelectorAll('.carousel img');
+
+function trocarImagem() {
+    imagens[index].classList.remove('active');
+
+    index++;
+    if (index >= imagens.length) {
+        index = 0;
+    }
+
+    imagens[index].classList.add('active');
+}
+
+// troca automática a cada 3 segundos
+setInterval(trocarImagem, 3000);
